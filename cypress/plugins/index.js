0086888ae4f3a1fbc1732ba4cedc3fca518116d2
Kashifch <aghaawais01@gmail.com>
2020-09-29 18:15:42 +0500

    index.js update x1

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -4,34 +4,34 @@ const fs = require('fs-extra')
 const path = require('path')
 const gmail = require("./gmail_reader")
 
-function getConfigurationByFile (file) {
-  const pathToConfigFile = path.resolve('.', 'config', `${file}.json`)
+// function getConfigurationByFile (file) {
+//   const pathToConfigFile = path.resolve('.', 'config', `${file}.json`)
 
-  return fs.readJson(pathToConfigFile)
-}
+//   return fs.readJson(pathToConfigFile)
+// }
 
-// This function is called when a project is opened or re-opened (e.g. due to
-// the project's config changing)
-module.exports = (on, config) => {
-
-  on('task', {
-    failed: require('cypress-failed-log/src/failed')(),
-    mailReader: gmail.readEmail
-  })
-  const file = config.env.configFile
-
-  return getConfigurationByFile(file)
-
-}
-
-
+// // This function is called when a project is opened or re-opened (e.g. due to
+// // the project's config changing)
 // module.exports = (on, config) => {
 
 //   on('task', {
-//         failed: require('cypress-failed-log/src/failed')(),
-//         mailReader: gmail.readEmail
-//       })
+//     failed: require('cypress-failed-log/src/failed')(),
+//     mailReader: gmail.readEmail
+//   })
+//   const file = config.env.configFile
 
-//   // `on` is used to hook into various events Cypress emits
-//   // `config` is the resolved Cypress config
+//   return getConfigurationByFile(file)
+
 // }
+
+
+module.exports = (on, config) => {
+
+  on('task', {
+        failed: require('cypress-failed-log/src/failed')(),
+        mailReader: gmail.readEmail
+      })
+
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
+}
