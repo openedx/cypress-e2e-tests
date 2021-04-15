a37cde2519c754514a0d318d894fbb2149126590
Lael Birch <lbirch@edx.org>
2021-04-14 21:24:31 -0400

    Use correct alert selector
    Refactor tests to be smaller and more self-explanatory

--- cypress/integration/admin_portal/tests/test_bulk_code_management.spec.js
+++ cypress/integration/admin_portal/tests/test_bulk_code_management.spec.js
@@ -2,10 +2,25 @@ import LandingPage from '../pages/landing_page'
 import CodeManagementPage from '../pages/enterprise_code_management'
 import EnterpriseCoupons from '../helpers/enterprise_coupons'
 
+const modalWindowLabelsAndText = {
+  unassignedCodes: 'Unassigned codes: ',
+  selectedCodes: 'Selected codes: ',
+  helpEmailText: 'To add more than one user, enter one email address per line.',
+  uploadCSVLabel: 'Upload Email Addresses',
+  csvHelpText: 'The file must be a CSV containing a single column of email addresses.',
+  maxNumbersEmailError: 'Unable to assign codesYou have 2 codes selected, but you entered 3 emails. Please try again.',
+  csvMaxEmailError: 'Unable to assign codesYou have 2 codes selected, but your file has 3 emails. Please try again.',
+  noEmailError: 'Unable to assign codesNo email addresses provided. Either manually enter email addresses or upload a CSV file.',
+}
+
+const MODAL_ERROR_ALERT_SELECTOR = '.alert.alert-danger'
+
 describe('landing page tests', function () {
   const landingPage = new LandingPage()
   const codeManagementDashboard = new CodeManagementPage()
   const coupons = new EnterpriseCoupons()
+  let coupon
+  let remainingRedemptions
 
   before(function () {
     cy.login_using_api(Cypress.env('ADMIN_USER_EMAIL'), Cypress.env('ADMIN_USER_PASSWORD'))
@@ -25,66 +40,72 @@ describe('landing page tests', function () {
     cy.visit('/')
     landingPage.goToEnterprise(Cypress.env('enterprise_name'))
     landingPage.openCodeManagement()
-  })
-
-  it('checks for the bulk assignment and revoking of the coupons', function () {
     cy.server()
     cy.route('GET', `**/${this.couponId}/codes/?code_filter=unassigned**`).as('results')
     coupons.fetchCouponReport(this.couponId).then((response) => {
       const couponReport = response.body
       const [couponName] = couponReport.match(/Test_Coupon_\w+/g)
-      this.couponName = couponName
-      codeManagementDashboard.getCoupon().contains(this.couponName).click()
+      coupon = couponName
+      codeManagementDashboard.getCoupon().contains(coupon).click()
     })
     // Asserts the redemption count of coupons before assignment
     cy.wait('@results').then((xhr) => {
       const responseBody = xhr.response.body
-      this.remainingRedemptions = (responseBody.count)
-      codeManagementDashboard.getCouponRows().should('have.length', this.remainingRedemptions.toString())
+      remainingRedemptions = (responseBody.count)
+      codeManagementDashboard.getCouponRows().should('have.length', remainingRedemptions.toString())
       // Check checkoxes for bulk assignment
-      for (let rowIndex = 1; rowIndex < this.remainingRedemptions; rowIndex += 1) {
+      for (let rowIndex = 1; rowIndex < remainingRedemptions; rowIndex += 1) {
         codeManagementDashboard.selectCodesForBulkAssignment(rowIndex)
       }
     })
     // Click on Bulk Assign Code Button
     codeManagementDashboard.getBulkActionButton().click()
-    // Verify Modal Window contents
+  })
+  it('has the correct inputs', () => {
     codeManagementDashboard.getModalWindow().within(() => {
-      const fileName = 'valid_emails.csv'
-      const fileSelector = '.choose-file-btn.btn~input'
-      const modalWindowLabelsAndText = {
-        unassignedCodes: 'Unassigned codes: ',
-        selectedCodes: 'Selected codes: ',
-        helpEmailText: 'To add more than one user, enter one email address per line.',
-        uploadCSVLabel: 'Upload Email Addresses',
-        csvHelpText: 'The file must be a CSV containing a single column of email addresses.',
-        maxNumbersEmailError: 'Unable to assign codesYou have 2 codes selected, but you entered 3 emails. Please try again.',
-        csvMaxEmailError: 'Unable to assign codesYou have 2 codes selected, but your file has 3 emails. Please try again.',
-        noEmailError: 'Unable to assign codesNo email addresses provided. Either manually enter email addresses or upload a CSV file.',
-      }
-      cy.get('.modal-title').should('have.text', this.couponName)
-      cy.get('.modal-body>div>p:nth-child(1)').should('have.text', modalWindowLabelsAndText.unassignedCodes + this.remainingRedemptions)
-      cy.get('.modal-body>div>p:nth-child(2)').should('have.text', modalWindowLabelsAndText.selectedCodes + (this.remainingRedemptions - 1))
+      cy.get('.modal-title').should('have.text', coupon)
+      cy.get('.modal-body>div>p:nth-child(1)').should('have.text', modalWindowLabelsAndText.unassignedCodes + remainingRedemptions)
+      cy.get('.modal-body>div>p:nth-child(2)').should('have.text', modalWindowLabelsAndText.selectedCodes + (remainingRedemptions - 1))
       cy.get('#email-addresses-help-text').should('have.text', modalWindowLabelsAndText.helpEmailText)
       cy.get('.file-input>label').should('have.text', modalWindowLabelsAndText.uploadCSVLabel)
       cy.get('#csv-email-addresses-help-text').should('have.text', modalWindowLabelsAndText.csvHelpText)
+      cy.get('.modal-title').should('have.text', coupon)
+    })
+  })
+  it('renders a max number of emails error', () => {
+    codeManagementDashboard.getModalWindow().within(() => {
       cy.get('textarea[name="email-addresses"]').type('cypress1@edx.org')
         .type('{enter}')
         .type('cypress2@edx.com')
         .type('{enter}')
         .type('cypress3@edx.org')
       cy.get('.modal-footer .btn:nth-of-type(2)').click()
-      cy.get('.alert-dialog .message').should('have.text', modalWindowLabelsAndText.maxNumbersEmailError)
+      cy.get('.alert.alert-danger').should('have.text', modalWindowLabelsAndText.maxNumbersEmailError)
       cy.get('textarea[name="email-addresses"]').clear()
+    })
+  })
+
+  it('renders a csv max emails error', () => {
+    codeManagementDashboard.getModalWindow().within(() => {
+      const fileSelector = '.choose-file-btn.btn~input'
+      const fileName = 'valid_emails.csv'
+
       cy.get(fileSelector).attachFile(fileName)
       cy.get('.file-name').should('have.text', 'valid_emails.csv')
       cy.get('.modal-footer .btn:nth-of-type(2)').click()
-      cy.get('.alert-dialog .message').should('have.text', modalWindowLabelsAndText.csvMaxEmailError)
+      cy.get(MODAL_ERROR_ALERT_SELECTOR).should('have.text', modalWindowLabelsAndText.csvMaxEmailError)
       cy.get('.remove-file-btn').click()
+    })
+  })
+  it('renders a no emails error', () => {
+    codeManagementDashboard.getModalWindow().within(() => {
       cy.get('.modal-footer .btn:nth-of-type(2)').click()
-      cy.get('.alert-dialog .message').should('have.text', modalWindowLabelsAndText.noEmailError)
-      cy.get('.modal-footer .btn:nth-of-type(2)').click()
-      cy.get('.alert-dialog .message').should('have.text', modalWindowLabelsAndText.noEmailError)
+      cy.get(MODAL_ERROR_ALERT_SELECTOR).should('have.text', modalWindowLabelsAndText.noEmailError)
+    })
+  })
+  it('checks for the bulk assignment and revoking of the coupons success', function () {
+    // Verify Modal Window contents
+    codeManagementDashboard.getModalWindow().within(() => {
       cy.get('textarea[name="email-addresses"]').type('{selectall}')
         .type('cypress1@edx.org')
         .type('{enter}')
@@ -100,7 +121,7 @@ describe('landing page tests', function () {
     // This assertion is skipped for now
     // Asserts the redemption count of coupons after the assignment
     // codeManagementDashboard.getCouponMeta().then((couponMeta) => {
-    //   expect((this.remainingRedemptions - 2).toString()).to.deep.eq(couponMeta.eq(3).text())
+    //   expect((remainingRedemptions - 2).toString()).to.deep.eq(couponMeta.eq(3).text())
     // })
     codeManagementDashboard.getCodeStatusFilter().select('Unredeemed')
     codeManagementDashboard.selectCodesForBulkRevoke()
