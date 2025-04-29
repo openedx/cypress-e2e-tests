b2bd68f9cf98ca35a375f658ebc3e3c4b4ca8209
Max Sokolski <cmltawt0@gmail.com>
2025-04-29 13:45:14 +0300

    chore: upgrade to Cypress 14.3.2
    Changes:
    - remove config files
    - move to env variables
    - migrate project structure to Cypress 10.0.0+ ver

---
+++ cypress.config.js
@@ -0,0 +1,33 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  viewportWidth: 1920,
+  viewportHeight: 1080,
+  defaultCommandTimeout: 10000,
+  requestTimeout: 10000,
+  pageLoadTimeout: 80000,
+  taskTimeout: 300000,
+  projectId: process.env.CYPRESS_PROJECT_ID,
+  video: false,
+  watchForFileChanges: true,
+  chromeWebSecurity: false,
+  retries: {
+    runMode: 2,
+    openMode: 2,
+  },
+  e2e: {
+    setupNodeEvents(on, config) {},
+    specPattern: 'cypress/e2e/auth/tests/**/*.{js,jsx,ts,tsx}',
+    baseUrl: process.env.BASE_URL,
+  },
+  blockHosts: [
+    "**.cdn.**",
+    "cdn.*",
+    "*.optimizely.**",
+    "www.googletagmanager.com",
+    "www.google-analytics.com",
+    "*.qualaroo.**",
+    "s3.amazonaws.com",
+    "*.nr-data.net**"
+  ],
+})
--- cypress/e2e/auth/tests/testLogin.cy.js
+++ cypress/e2e/auth/tests/testLogin.cy.js
@@ -1,29 +1,29 @@
 import LoginPage from '../pages/loginPage'
 
-describe('Login page tests', function () {
-  const loginPage = new LoginPage()
+const loginPage = new LoginPage()
 
-  before(function () {
+describe('Login page tests', () => {
+  before(() => {
     cy.clearCookies()
   })
 
-  beforeEach(function () {
+  beforeEach(() => {
     cy.visit('/login')
   })
 
-  it('should show empty field error messages', function () {
+  it('should show empty field error messages', () => {
     loginPage.clickSubmit()
     loginPage.getLoginFailureError().should('contain.text', 'We couldn\'t sign you in.')
     loginPage.getLoginFailureError().should('contain.text', 'Please fill in the fields below.')
   })
 
-  it('should show invalid email or password error message', function () {
+  it('should show invalid email or password error message', () => {
     loginPage.loginUser('incorrect@email.com', 'incorrect-password')
     loginPage.getLoginFailureError().should('contain.text', 'We couldn\'t sign you in.')
     loginPage.getLoginFailureError().should('contain.text', 'The username, email, or password you entered is incorrect. Please try again.')
   })
 
-  it('user can successfully login and redirected to dashboard', function () {
+  it('user can successfully login and redirected to dashboard', () => {
     loginPage.loginUser(Cypress.env('ADMIN_USER_EMAIL'), Cypress.env('ADMIN_USER_PASSWORD'))
     cy.location('hostname').should('eq', 'home.stage.edx.org')
   })
