7b02f185a45d62700f326549825b4f669d18aa77
Iryna <iryna.zaporozhets@raccoongang.com>
2026-01-23 16:26:05 +0200

    feat: [CC-148] fixing advanced settings logic +fix lint issues

--- cypress/e2e/studio/settings/testAdvancedSettings.cy.js
+++ cypress/e2e/studio/settings/testAdvancedSettings.cy.js
@@ -1,7 +1,8 @@
 import AdvancedSettings from '../../../pages/studio/settings/advancedSettings'
 import ScheduleAndDetails from '../../../pages/studio/settings/scheduleAndDetails'
 import CourseDiscoveryPage from '../../../pages/lms/courseDiscoveryPage'
-import { NEW_COURSE_DATA } from '../../../support/constants'// '../../../../../support/constants'
+import CourseAboutPage from '../../../pages/lms/courseAboutPage'
+import { NEW_COURSE_DATA } from '../../../support/constants'
 
 describe('[TC_AUTHOR_131] Course display name', function () {
   it.skip('Course display name (remember to reindex course in /courses page)', () => {
@@ -17,8 +18,12 @@ describe('[TC_AUTHOR_133] Test course visibility in catalog', { tags: '@smoke' }
   const advancedSettings = new AdvancedSettings()
   const scheduleAndDetails = new ScheduleAndDetails()
   const courseDiscoveryPage = new CourseDiscoveryPage()
+  const aboutCoursePage = new CourseAboutPage()
   const baseMFEURL = Cypress.env('BASE_MFE_URL')
   const { courseId } = NEW_COURSE_DATA
+  let startDateTime
+  let endDateTime
+  let startEnrollDateTime
 
   before(function () {
     cy.clearCookies()
@@ -27,36 +32,69 @@ describe('[TC_AUTHOR_133] Test course visibility in catalog', { tags: '@smoke' }
   beforeEach(function () {
     cy.loginAdminLmsCms()
     cy.visit(`${baseMFEURL}/authoring/course/${courseId}`)
+
+    // Set course dates for all tests
+    startDateTime = scheduleAndDetails.getDateWithOffset(0)
+    endDateTime = scheduleAndDetails.getDateWithOffset(30)
+    startEnrollDateTime = scheduleAndDetails.getDateWithOffset(-1)
+    scheduleAndDetails.setCourseDates(courseId, startDateTime, endDateTime, startEnrollDateTime)
   })
 
-  it.skip('Test course visibility = `none` behavior', function () {
+  it('Test course visibility = `none` behavior', function () {
+    advancedSettings.courseVisibility(courseId, 'none')
+      .then(() => scheduleAndDetails.searchReindex(courseId))
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      .then(() => cy.wait(2000)) // reduce flakiness
+      .then(() => {
+        cy.clearAllCookies()
+        cy.signin('test user', Cypress.env('LMS_USER_EMAIL'), Cypress.env('LMS_USER_PASSWORD'))
+        cy.visit('/courses/')
+        courseDiscoveryPage.searchCourse(NEW_COURSE_DATA.courseName).then(() => {
+          courseDiscoveryPage.getCourseNotExists(NEW_COURSE_DATA.courseName)
+        })
+      })
   })
 
-  it.skip('Test course visibility = `about` behavior', function () {
+  it('Test course visibility = `about` behavior', function () {
+    advancedSettings.courseVisibility(courseId, 'about')
+      .then(() => scheduleAndDetails.searchReindex(courseId))
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      .then(() => cy.wait(2000)) // reduce flakiness
+      .then(() => {
+        cy.clearAllCookies()
+        cy.signin('test user', Cypress.env('LMS_USER_EMAIL'), Cypress.env('LMS_USER_PASSWORD'))
+        cy.visit('/courses/')
+        courseDiscoveryPage.searchCourse(NEW_COURSE_DATA.courseName).then(() => {
+          courseDiscoveryPage.getCourseNotExists(NEW_COURSE_DATA.courseName)
+          cy.visit(`/courses/${courseId}/about`)
+          aboutCoursePage.checkCourseName(NEW_COURSE_DATA.courseName)
+          aboutCoursePage.getEnrollNowButton()
+            .should('contain.text', 'Enroll Now')
+        })
+      })
   })
 
   it('Test course visibility = `both` behavior', function () {
-    const startDateTime = scheduleAndDetails.getDateWithOffset(0)
-    const endDateTime = scheduleAndDetails.getDateWithOffset(30)
-
-    scheduleAndDetails.setStartDate(courseId, startDateTime)
-    scheduleAndDetails.setEndDate(courseId, endDateTime)
-
     advancedSettings.courseVisibility(courseId, 'both')
-
-    cy.signin('test user', Cypress.env('LMS_USER_EMAIL'), Cypress.env('LMS_USER_PASSWORD'))
-    cy.visit('/courses/')
-    const formattedDate = scheduleAndDetails.getFormattedDate(startDateTime)
-    courseDiscoveryPage.searchCourse(NEW_COURSE_DATA.courseName).then(() => {
-      courseDiscoveryPage.verifyCourseAfterSearch(
-        NEW_COURSE_DATA.courseName,
-        NEW_COURSE_DATA.courseOrg,
-        NEW_COURSE_DATA.courseCode,
-        formattedDate,
-      )
-      courseDiscoveryPage.selectCourse(NEW_COURSE_DATA.courseName)
-      cy.url().should('include', `/courses/${courseId}/about`)
-    })
+      .then(() => scheduleAndDetails.searchReindex(courseId))
+      // eslint-disable-next-line cypress/no-unnecessary-waiting
+      .then(() => cy.wait(2000)) // reduce flakiness
+      .then(() => {
+        cy.clearAllCookies()
+        cy.signin('test user', Cypress.env('LMS_USER_EMAIL'), Cypress.env('LMS_USER_PASSWORD'))
+        cy.visit('/courses/')
+        const formattedDate = scheduleAndDetails.getFormattedDate(startDateTime)
+        courseDiscoveryPage.searchCourse(NEW_COURSE_DATA.courseName).then(() => {
+          courseDiscoveryPage.verifyCourseAfterSearch(
+            NEW_COURSE_DATA.courseName,
+            NEW_COURSE_DATA.courseOrg,
+            NEW_COURSE_DATA.courseCode,
+            formattedDate,
+          )
+          courseDiscoveryPage.selectCourse(NEW_COURSE_DATA.courseName)
+          cy.url().should('include', `/courses/${courseId}/about`)
+        })
+      })
   })
 })
 
